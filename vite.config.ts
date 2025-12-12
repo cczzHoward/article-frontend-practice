593b0fa3dbeb83f1bc156a703f2d50c56737a258
cczzHoward <howardgg509@gmail.com>
2025-12-12 16:44:23 +0800

    ci: 新增 GitHub Pages 自動化部署流程

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/article-frontend-practice/',
     resolve: {
         alias: {
             '@app': path.resolve(__dirname, './src'),
