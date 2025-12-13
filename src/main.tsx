51fa6a018da0c6f5b9d40639a5a6c91730384384
cczzHoward <howardgg509@gmail.com>
2025-12-13 14:10:59 +0800

    fix: 將 BrowserRouter 替換為 HashRouter 以進行路由管理

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './styles/index.css';
 
@@ -11,8 +11,8 @@ const basename = import.meta.env.PROD
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter basename={basename}>
+        <HashRouter basename={basename}>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
